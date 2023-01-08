2459f9ab76c9a3926047d996d7bd368a739272bd
Mateusz-11 <tweak.night@gmail.com>
2023-01-08 22:22:02 +0100

    Solving tasks - module 2

---
+++ modul-2/tuple1.py
@@ -0,0 +1,8 @@
+print(('owca'))
+print(('owca', ))
+print(type(('owca')))
+print(type(('owca', )))
+
+fruits = ('lemon', 'apple', 'peach', 'kivi', 'mango')
+
+print(fruits[0], fruits[2], fruits[4])
---
+++ modul-2/tuple2.py
@@ -0,0 +1,9 @@
+list_of_names = ('Ann', 'Mathew', 'Peter', 'John')
+
+name = input("What's your name?")
+
+if name in list_of_names:
+    print('Welcome!')
+else:
+    print('We do not know you!')
+
