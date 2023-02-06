cc63c2876df7be08c40f32d9a06a72da25c2e901
Mateusz-11 <tweak.night@gmail.com>
2023-02-06 21:31:09 +0100

    Solving tasks - module 3

--- modul-3/dictionaries.py
+++ modul-3/dictionaries.py
@@ -1,11 +1,34 @@
-dict_ang_pl = {"dog": "pies", "cat": "kot", "snake": "wąż", "tiger": "tygrys"}
-choice = input("Wybierz język (ang/pl): ")
+dict_ang_pl = {
+    'dog': 'pies',
+    'cat': 'kot',
+    'snake': 'wąż',
+    'tiger': 'tygrys'
+}
+
+choice = input("Wybierz język (ang / pl): ")
+
+if choice not in ['ang', 'pl']:
+    print("Takie tłumaczenie jest niedostępne")
+    quit()
 
 if choice == "ang":
     ask_word = input("Jakie słowo przetłumaczyć na polski? ")
-    for k, v in dict_ang_pl:
-        if ask_word in dict_ang_pl.keys() or ask_word in dict_ang_pl.values():
-            print(dict_ang_pl.get(ask_word))
-        else:
-            print("Nie ma takiego słowa w słowniku.")
+    for k, v in dict_ang_pl.items():
+        if k == ask_word:
+            print(f"Odpowiedź to: {v}")
+            break
+    else:
+        print("Nie ma takiego słowa w słowniku.")
+        quit()
+elif choice == "pl":
+    ask_word = input("Jakie słowo przetłumaczyć na angielski? ")
+    for k, v in dict_ang_pl.items():
+        if v == ask_word:
+            print(f"Odpowiedź to: {k}")
+            break
+    else:
+        print("Nie ma takiego słowa w słowniku.")
+        quit()
+else:
+    print("Takie tłumaczenie jest niedostępne")
 
