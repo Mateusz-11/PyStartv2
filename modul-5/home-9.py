9a89a7e37124affd5a9fb636e182e6e825303039
Mateusz-11 <tweak.night@gmail.com>
2023-02-23 20:16:56 +0100

    Solving tasks - module 5

---
+++ modul-5/home-9.py
@@ -0,0 +1,8 @@
+def reverse_text(txt:str) -> str:
+    new_txt = txt.split(' ')
+    result = []
+    for i in new_txt:
+        result.append(i[::-1])
+    return " ".join(result)
+
+print(reverse_text("programuję w pythonie"))
