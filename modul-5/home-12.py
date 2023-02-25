4c60e34624adc4e75c0afd92d2925f23a7b9d2dd
Mateusz-11 <tweak.night@gmail.com>
2023-02-25 21:16:00 +0100

    Solving tasks - module 5

--- modul-5/home-12.py
+++ modul-5/home-12.py
@@ -1,2 +1,8 @@
 def convert_str_to_int(list_strings:list) ->list:
     return list(map(int, list_strings))
+
+def convert_int_to_str(list_int:list) -> list:
+    return list(map(str, list_int))
+
+print(convert_int_to_str([1,2,3,4]))
+print(convert_str_to_int(['1','2','3','4']))
