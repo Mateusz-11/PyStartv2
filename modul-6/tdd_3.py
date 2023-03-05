8f88e6d1e3fe930e038e74372393eac5824b57ac
Mateusz-11 <tweak.night@gmail.com>
2023-03-05 21:53:24 +0100

    Solving tasks - module 6

--- modul-6/tdd_3.py
+++ modul-6/tdd_3.py
@@ -1,19 +1,22 @@
 import pytest
 
 
-def create_list_of_words(text: str) -> list:
-    result = []
+def create_list_of_words(text: str) -> set:
+    result = set()
     vowel_list = ['i', 'y', 'e', 'a', 'o', 'u', 'ą', 'ę']
-    new_text = text.split()
+    new_text = text.split(' ')
     for i in new_text:
-        if i[0] not in vowel_list and i[-1] in vowel_list:
-            result.append(i)
+        if i[0].lower() not in vowel_list and i[-1].lower() in vowel_list:
+            result.add(i)
     return result
 
 
 @pytest.mark.parametrize("text,expected", [
-    ("ala ma kota", ['ma', 'kota']),
-    ("ala ma nowy dom", ['ma', 'nowy']),
+    ("Ala ma kota", {'ma', 'kota'}),
+    ("Ala MA KOTA", {'MA', 'KOTA'}),
+    ("Ala Ma KOTA", {'Ma', 'KOTA'}),
+    ("ala ma kota", {'ma', 'kota'}),
+    ("Ala ma nowy dom", {'ma', 'nowy'}),
 
 ])
 def test_search_words(text, expected):
