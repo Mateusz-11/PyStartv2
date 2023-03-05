aa159f51860c19144883c455dc91bae7438dcdcd
Mateusz-11 <tweak.night@gmail.com>
2023-03-05 21:46:31 +0100

    Solving tasks - module 6

--- modul-6/tdd_2.py
+++ modul-6/tdd_2.py
@@ -6,13 +6,18 @@ def create_list_of_numbers(n: int, m: int, z: int) -> list:
     for i in range(n, m + 1):
         if i % z == 0:
             result.append(i)
+
     return result
 
 
 @pytest.mark.parametrize("n_number,m_number, z_number,expected", [
     (6, 20, 2, [6, 8, 10, 12, 14, 16, 18, 20]),
+    (2, 10, 2, [2, 4, 6, 8, 10]),
+    (2, 10, 30, []),
+    (3, 12, 2, [4, 6, 8, 10, 12]),
 
 ])
 def test_divider(n_number, m_number, z_number, expected):
     divider = create_list_of_numbers(n_number, m_number, z_number)
     assert divider == expected
+    assert isinstance(create_list_of_numbers(n_number, m_number, z_number), list)
