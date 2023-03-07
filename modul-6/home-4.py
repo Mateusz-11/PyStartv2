2904c2414a691cd5af75f4ccb12db8261bd88168
Mateusz-11 <tweak.night@gmail.com>
2023-03-07 19:41:23 +0100

    Solving tasks - module 6

--- modul-6/home-4.py
+++ modul-6/home-4.py
@@ -1,3 +1,25 @@
-def check_pesel()
-    def count_words(sentence: str, word: str) -> int:
-        return sentence.count(word)
+def check_pesel(pesel: str):
+    CONTROL = '13791379131'
+
+    if len(pesel) != 11:
+        return False
+
+    control_sum = 0
+    for pesel_char, control_char in zip(pesel, CONTROL):
+        control_sum += int(pesel_char) * int(control_char)
+
+    if control_sum % 10 != 0:
+        return False
+
+    return True
+
+
+def test_check_pesel_right():
+    assert check_pesel('82012974188')
+    assert check_pesel('06281074994')
+
+
+def test_check_pesel_wrong():
+    assert not check_pesel('123')
+    assert not check_pesel('06283074994')
+    assert not check_pesel('123789789456')
