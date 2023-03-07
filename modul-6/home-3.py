9eced19b861352519450a7694bab9f93461c3b08
Mateusz-11 <tweak.night@gmail.com>
2023-03-07 19:22:48 +0100

    Solving tasks - module 6

--- modul-6/home-3.py
+++ modul-6/home-3.py
@@ -1,7 +1,14 @@
 def count_words(sentence: str, word: str) -> int:
-    return sentence.count(word)
-
+    # return sentence.count(word)
+    counter = 0
+    for i in sentence.split(' '):
+        if i == word:
+            counter += 1
+    return counter
 
 def test_count_words():
     assert count_words('Python i Python', 'Python') == 2
-    assert count_words('"I love apples, apple are my favorite fruit"', 'apple') == 2
+    assert count_words('Python i Python', 'python') == 0
+    assert count_words('Python i python', 'python') == 1
+    assert count_words('Java i PHP', 'Python') == 0
+    assert count_words('"I love apples, apple are my favorite fruit"', 'apple') == 1
---
+++ modul-6/home-4.py
@@ -0,0 +1,3 @@
+def check_pesel()
+    def count_words(sentence: str, word: str) -> int:
+        return sentence.count(word)
