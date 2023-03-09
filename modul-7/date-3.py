37a72970b27aed2c2f083cd9b1f2c01684d39548
Mateusz-11 <tweak.night@gmail.com>
2023-03-09 22:51:47 +0100

    Solving tasks - module 7

--- modul-7/date-3.py
+++ modul-7/date-3.py
@@ -5,9 +5,12 @@ date_end = datetime.strptime(input('Data zakonczenia dd.mm.rrrr: '), '%d.%m.%Y')
 day_salary = int(input('Pensja dzienna: '))
 
 diff = date_end - date_start
+salary = (diff.days + 1) * day_salary
 
 while (date_start <= date_end):
-    print(date_start)
+    if date_start.strftime('%a') in ['Sat', 'Sun']:
+        salary += day_salary
+    print(date_start.strftime('%a%d.%m.%Y'))
     date_start += timedelta(days=1)
 
-print(f'Pracownik zarobi: {day_salary * diff.days}')
+print(f'Pracownik zarobi: {salary}')
