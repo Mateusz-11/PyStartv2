2270f9c8d30d48690cb9e3842e4f0285c7ded524
Mateusz-11 <tweak.night@gmail.com>
2023-03-11 18:56:23 +0100

    Solving tasks - module 7

--- modul-7/files-2.py
+++ modul-7/files-2.py
@@ -1,24 +1,18 @@
-income = []
-
-with open('transakcje.txt') as file:
-    for line in file:
-        date, description, amount = line.strip().split(';')
-        if int(amount) > 0:
-            income.append({"amount": int(amount)})
-
-# print(income)
-with open('przychody.txt', mode='w') as file:
-    for amount in income:
-        file.write(f"{amount.get('amount')}\n")
+with open('transakcje.txt', encoding='utf8') as file, open('przychody.txt', encoding='utf8', mode='w') as output:
+    for transaction in file:
+        date_of_transaction, description, amount = transaction.strip().split(';')
+        amount = float(amount)
+        if amount > 0:
+            output.write(transaction)
 
 
 def sum_of_income():
-    result = 0
-    with open('przychody.txt') as file1:
-        for line1 in file1:
-            data = line1.strip()
-            result += int(data)
-    print(f'Sum of income: {result} zl')
+    total = 0
+    with open('przychody.txt', encoding='utf8') as income_file:
+        for line in income_file:
+            _,_, amount = line.strip().split(';')
+            total += float(amount)
+    print(f'Sum of income: {total} zl')
 
 
 sum_of_income()
