37bb8e24d429774c983ded351d0f218b88915b57
Mateusz-11 <tweak.night@gmail.com>
2023-03-12 17:07:25 +0100

    Solving tasks - module 7

---
+++ modul-7/json-1.py
@@ -0,0 +1,19 @@
+from json import load, dump
+
+new_author = "Witold Gombrowicz"
+new_title = "Ferdydurke"
+
+with open('book.json') as file:
+    books = load(file)
+
+for book in books:
+    print(book.get('title'))
+    print(book.get('author'))
+    print('---' * 10)
+
+with open('book.json', 'w') as file:
+    books.append({
+        'title': new_title,
+        'author': new_author,
+    })
+    dump(books, file)
