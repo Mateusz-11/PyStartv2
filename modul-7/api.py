beb22bb2aa9e20d5796f3a9fd77cb78b104b286c
Mateusz-11 <tweak.night@gmail.com>
2023-03-14 19:17:00 +0100

    Solving tasks - module 7

--- modul-7/api.py
+++ modul-7/api.py
@@ -1,3 +1,9 @@
-from request import get
+from requests import get
 
-url = 'http://api.nbp.pl/api/exchangerates/tables/'
+url = 'http://api.nbp.pl/api/exchangerates/tables/A'
+codes = ['EUR', 'USD', 'GBP']
+response = get(url)
+data = response.json()
+for rate in data[0]['rates']:
+    if rate['code'] in codes:
+        print(rate['currency'], rate['mid'])
