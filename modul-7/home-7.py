d6b01f5962f96ff7e67e86ec01b3448c899ab55e
Mateusz-11 <tweak.night@gmail.com>
2023-03-15 16:44:50 +0100

    Solving tasks - module 7

--- modul-7/home-7.py
+++ modul-7/home-7.py
@@ -1,3 +1,9 @@
+# Do zrobienia:
+# 1. Zapis do json
+# 2. Zapis z nazwą pliku ze slugu
+# 3. Pobieranie danych po slugu
+# 4. DOdanie wyjątku, jeśli nei znajdzie w bazie
+
 from requests import get
 
 url = 'https://wolnelektury.pl/api/books'
@@ -6,6 +12,14 @@ wanted_author = "Ajschylos"
 response = get(url)
 data = response.json()
 
+books = []
+
 for book in data:
     if book['author'] == wanted_author:
         print(f"Książka: {book['title']}")
+        books.append(book['title'].strip())
+
+
+with open('1-test.txt', 'w', encoding='utf8') as file:
+    file.write('\n'.join(books))
+
